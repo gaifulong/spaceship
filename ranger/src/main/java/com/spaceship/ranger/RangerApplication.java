017be04516781ae735e6259b039aa0b94cf8b7f7
gaifulong <gfl7612@hotmail.com>
2020-07-27 11:18:16 +0800

    测试

---
+++ ranger/src/main/java/com/spaceship/ranger/RangerApplication.java
@@ -0,0 +1,13 @@
+package com.spaceship.ranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RangerApplication.class, args);
+    }
+
+}
---
+++ ranger/src/test/java/com/spaceship/ranger/RangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceship.ranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
